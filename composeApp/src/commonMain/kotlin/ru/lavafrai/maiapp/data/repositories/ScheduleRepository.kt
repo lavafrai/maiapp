99b812768186764c2916d83c21c7d69cccc76e49
lavafrai <lavafrai@yandex.ru>
2024-11-27 20:15:53 +0300

    Indev: - Week selector Done: - Schedule caching optimized

--- composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/data/repositories/ScheduleRepository.kt
+++ composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/data/repositories/ScheduleRepository.kt
@@ -2,10 +2,11 @@
 
 package ru.lavafrai.maiapp.data.repositories
 
-import com.russhwolf.settings.serialization.decodeValueOrNull
-import com.russhwolf.settings.serialization.encodeValue
+import com.russhwolf.settings.set
 import io.ktor.client.*
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import ru.lavafrai.maiapp.models.schedule.Schedule
 import ru.lavafrai.maiapp.network.MaiApi
 import ru.lavafrai.maiapp.platform.getPlatformSettingsStorage
@@ -18,11 +19,23 @@ class ScheduleRepository(
     private val cache = getPlatformSettingsStorage()
 
     suspend fun getSchedule(name: String) = withCache("schedule:$name") { api.schedule(name) }
-    suspend fun getScheduleFromCacheOrNull(name: String) = cache.decodeValueOrNull<Schedule>("schedule:$name")
+    suspend fun getScheduleFromCacheOrNull(name: String) = fromCache<Schedule>("schedule:$name")
 
     suspend fun withCache(key: String, block: suspend () -> Schedule): Schedule {
         val data = block()
-        cache.encodeValue(key, data)
+        val encoded = Json.encodeToString(data)
+        cache.set(key, encoded)
+        return data
+    }
+
+    private suspend inline fun <reified T> fromCache(key: String): T? {
+        val encoded = cache.getStringOrNull(key)
+        val data = if (encoded != null) try {
+            Json.decodeFromString<T>(encoded!!)
+        } catch (e: Exception) {
+            null
+        } else null
+
         return data
     }
 }
