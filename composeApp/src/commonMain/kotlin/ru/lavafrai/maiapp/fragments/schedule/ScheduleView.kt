afe4a1b17200c4725ba8df05ada6a890c94c0b23
lavafrai <lavafrai@yandex.ru>
2025-02-13 03:48:48 +0300

    feat: pull to refresh when schedule refreshes

--- composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/fragments/schedule/ScheduleView.kt
+++ composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/fragments/schedule/ScheduleView.kt
@@ -11,8 +11,11 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -119,8 +122,14 @@ fun ScheduleView(
             }
         }
 
+        val hapticFeedback = LocalHapticFeedback.current
+
         if (onRefresh != null) PullToRefreshBox(
-            onRefresh = { refreshing = true ; scope.launch { refresh() } },
+            onRefresh = {
+                hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                refreshing = true
+                scope.launch { refresh() }
+            },
             isRefreshing = refreshing,
         ) {
             content()
