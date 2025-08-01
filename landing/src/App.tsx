514986d6675aab75f501838650d7f71e2f5c5978
lavafrai <lavafrai@yandex.ru>
2025-08-02 01:51:43 +0300

    feat: indev waf

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/home/HomePage.tsx";
 import DownloadPage from "./pages/download/DownloadPage.tsx";
+import NotFound from "./pages/notfound/NotFound.tsx";
 
 function App() {
     // const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path='*' element={<NotFound/>} />
                 <Route path="/" element={<HomePage/>}></Route>
                 <Route path="/download" element={<DownloadPage/>}></Route>
             </Routes>
