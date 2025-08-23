2dd2a45c7c34485251d2557a63b208d8cb093812
lavafrai <lavafrai@yandex.ru>
2025-08-23 23:00:03 +0300

    feat: integrate Yandex Metrika for analytics tracking

--- landing/src/App.tsx
+++ landing/src/App.tsx
@@ -2,18 +2,22 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/home/HomePage.tsx";
 import DownloadPage from "./pages/download/DownloadPage.tsx";
 import NotFound from "./pages/notfound/NotFound.tsx";
+import {YMInitializer} from "react-yandex-metrika";
 
 function App() {
     // const [count, setCount] = useState(0)
 
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='*' element={<NotFound/>} />
-                <Route path="/" element={<HomePage/>}></Route>
-                <Route path="/download" element={<DownloadPage/>}></Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            <YMInitializer accounts={[98659410]} />
+            <BrowserRouter>
+                <Routes>
+                    <Route path='*' element={<NotFound/>}/>
+                    <Route path="/" element={<HomePage/>}></Route>
+                    <Route path="/download" element={<DownloadPage/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </>
     )
 }
 
