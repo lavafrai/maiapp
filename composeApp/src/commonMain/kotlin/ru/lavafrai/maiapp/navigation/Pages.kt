afc578e1ad78b61246b953c02aa8ee8d2d59df11
lavafrai <lavafrai@yandex.ru>
2026-05-17 14:58:42 +0300

    Fix iOS build dependencies

--- composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/navigation/Pages.kt
+++ composeApp/src/commonMain/kotlin/ru/lavafrai/maiapp/navigation/Pages.kt
@@ -1,27 +1,27 @@
 package ru.lavafrai.maiapp.navigation
 
-import androidx.core.bundle.Bundle
 import androidx.navigation.NavType
-import kotlinx.serialization.Serializable
+import androidx.savedstate.SavedState
+import androidx.savedstate.read
+import androidx.savedstate.write
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import ru.lavafrai.maiapp.viewmodels.login.LoginTarget
 import ru.lavafrai.maiapp.viewmodels.login.LoginType
-import kotlin.jvm.JvmStatic
 
 
 inline fun <reified T> navTypeOf(
     isNullableAllowed: Boolean = false,
     json: Json = Json,
 ) = object : NavType<T>(isNullableAllowed = isNullableAllowed) {
-    override fun get(bundle: Bundle, key: String): T? =
-        bundle.getString(key)?.let(json::decodeFromString)
+    override fun get(bundle: SavedState, key: String): T? =
+        bundle.read { if (!contains(key) || isNull(key)) null else getString(key) }
+            ?.let(json::decodeFromString)
 
     override fun parseValue(value: String): T = json.decodeFromString(value)
 
     override fun serializeAsValue(value: T): String = json.encodeToString(value)
 
-    override fun put(bundle: Bundle, key: String, value: T) =
-        bundle.putString(key, json.encodeToString(value))
-
+    override fun put(bundle: SavedState, key: String, value: T) =
+        bundle.write { putString(key, json.encodeToString(value)) }
 }
